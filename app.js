513c333d0dab425b252eca32d71a2dccc2b86567
J T <jtasharski@gmail.com>
2015-10-24 18:12:31 -0700

    directives added

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 angular.module('leGo', [])
-.controller('MainCtrl', ['$scope', function($scope){
-	$scope.award = "a word";
-}])
+	.controller('MainCtrl', ['$scope', function($scope){
+		$scope.award = "a word";
+	}])
+	.directive("brick", Brick)
+	.directive("slot", Slot);
 //// other controllers
