57fd8afa5ca76802e493e138fdc01bb2fc5f0da4
Allxie <allxiecleary@gmail.com>
2018-05-21 20:38:44 -0700

    factors out controller and some constants out fo app.js

---
+++ constants/colors.js
@@ -0,0 +1,20 @@
+var colors = [{
+       name: 'Red',
+       value: 'red'
+    }, {
+       name: 'Orange',
+       value: 'orange'
+    },{
+       name: 'Yellow',
+       value: 'yellow'
+    }, {
+       name: 'Green',
+       value: 'green'
+    },{
+       name: 'Blue',
+       value: 'blue'
+    }, {
+       name: 'Purple',
+       value: 'purple'
+    }
+];
---
+++ constants/trash.js
@@ -0,0 +1,9 @@
+var trash = function(singleWidth){
+return { // this is the data for the trash brick
+      "id": "trash",
+      "label" : "Delete",
+      "color" : "white",
+      "pegs" : 2,
+      "width": (singleWidth * 2) + "px", "occupied": false
+    }
+};
